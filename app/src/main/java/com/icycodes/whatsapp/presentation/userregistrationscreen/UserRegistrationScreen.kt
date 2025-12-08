dda6a5ac5165a3853cb7af9eb9719cebe61f8cc8
icy <piyushlikhitkar1999@gmail.com>
2025-12-08 23:00:07 +0530

    User Registration Screen

---
+++ app/src/main/java/com/icycodes/whatsapp/presentation/userregistrationscreen/UserRegistrationScreen.kt
@@ -0,0 +1,171 @@
+package com.icycodes.whatsapp.presentation.userregistrationscreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.icycodes.whatsapp.R
+
+@Composable
+@Preview(showSystemUi = true, showBackground = true)
+fun UserRegistrationScreen() {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    var selectedCountry by remember { mutableStateOf("India") }
+
+    var countryCode by remember { mutableStateOf("+91") }
+    var phoneNumber by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Enter your phone number",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            color = colorResource(R.color.dark_green)
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row {
+            Text("WhatsApp wil need to verify your phone number. ", color = Color.Gray)
+            Text("What's", color = colorResource(R.color.dark_green))
+        }
+        Text(" my number ?", color = colorResource(R.color.dark_green))
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        TextButton(onClick = { expanded = true }, modifier = Modifier.fillMaxWidth()) {
+            Box(modifier = Modifier.width(230.dp)) {
+                Text(
+                    text = selectedCountry,
+                    modifier = Modifier.align(Alignment.Center),
+                    fontSize = 16.sp,
+                    color = Color.Black
+                )
+
+                Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = null,
+                    modifier = Modifier.align(
+                        Alignment.CenterEnd
+                    ),
+                    tint = colorResource(R.color.light_green)
+                )
+            }
+        }
+        HorizontalDivider(
+            modifier = Modifier.padding(horizontal = 66.dp),
+            thickness = 2.dp,
+            color = colorResource(R.color.light_green)
+        )
+
+        DropdownMenu(expanded = expanded, onDismissRequest = {
+            expanded = false
+        }, modifier = Modifier.fillMaxWidth()) {
+            listOf(
+                "Japan", "USA", "China", "Canada"
+            ).forEach { country ->
+                DropdownMenuItem(text = { Text(text = country) }, onClick = {
+                    selectedCountry = country
+                    expanded = false
+                })
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row {
+                TextField(
+                    value = countryCode,
+                    onValueChange = {
+                        countryCode = it
+                    },
+                    modifier = Modifier.width(70.dp),
+                    singleLine = true,
+                    textStyle = LocalTextStyle.current.copy(fontSize = 18.sp),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedIndicatorColor = colorResource(R.color.light_green),
+                        focusedIndicatorColor = colorResource(R.color.light_green)
+
+                    )
+                )
+                Spacer(modifier = Modifier.width(6.dp))
+                TextField(
+                    value = phoneNumber,
+                    onValueChange = {
+                        phoneNumber = it
+                    }, placeholder = {
+                        Text(text = "phone number")
+                    },
+                    singleLine = true,
+                    textStyle = LocalTextStyle.current.copy(fontSize = 18.sp),
+                    colors = TextFieldDefaults.colors(
+                        unfocusedContainerColor = Color.Transparent,
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedIndicatorColor = colorResource(R.color.light_green),
+                        focusedIndicatorColor = colorResource(R.color.light_green)
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Carrier charges may apply",
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
+                fontSize = 12.sp
+            )
+
+            Spacer(modifier = Modifier.height(26.dp))
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(6.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.dark_green))
+            ) {
+                Text(text = "Next", fontSize = 16.sp)
+            }
+        }
+    }
+}
--- app/src/main/java/com/icycodes/whatsapp/presentation/welcomescreen/WelcomeScreen.kt
+++ app/src/main/java/com/icycodes/whatsapp/presentation/welcomescreen/WelcomeScreen.kt
@@ -1,12 +1,90 @@
 package com.icycodes.whatsapp.presentation.welcomescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.icycodes.whatsapp.R
 
 @Composable
 @Preview(showSystemUi = true)
-fun WelcomeScreen(){
-    Column(modifier = Modifier) {  }
+fun WelcomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.whatsapp_sticker),
+            contentDescription = null,
+            modifier = Modifier.size(300.dp)
+        )
+
+
+        Text("Welcome to WhatsApp", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row {
+            ReadText("Read our ")
+            ClickableText("Privacy Policy")
+            ReadText("Tap Agree and continue to")
+        }
+        Row {
+            ReadText("accept the ")
+            ClickableText("Terms of Service")
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        AgreeButton()
+    }
+}
+
+@Composable
+fun ReadText(text: String) {
+    Text(text = text, color = Color.Gray)
+}
+
+@Composable
+fun ClickableText(text: String, onClick: () -> Unit = {}) {
+    Text(
+        text = text,
+        color = colorResource(R.color.light_green),
+        modifier = Modifier.clickable { onClick() })
+
+}
+
+@Composable
+private fun AgreeButton(onClick: () -> Unit = {}) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(280.dp)
+            .height(45.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = colorResource(R.color.dark_green),
+            contentColor = Color.White
+        )
+    ) {
+        Text("Agree and Continue", fontSize = 16.sp)
+    }
 }
