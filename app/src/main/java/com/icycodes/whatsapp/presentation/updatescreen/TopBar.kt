d694359e86801f6fd55e485151338b8198d3bb80
icy <piyushlikhitkar1999@gmail.com>
2025-12-18 01:15:48 +0530

    Top Bar with functionality

---
+++ app/src/main/java/com/icycodes/whatsapp/presentation/updatescreen/TopBar.kt
@@ -0,0 +1,83 @@
+package com.icycodes.whatsapp.presentation.updatescreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.icycodes.whatsapp.R
+
+@Composable
+fun TopBar(pageName: String, colorFlag: Boolean) {
+    var isSearching by remember {
+        mutableStateOf(false)
+    }
+
+    if (isSearching) {
+        SearchBar(onCancel = { isSearching = false })
+        return
+    }
+    Box(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            pageName,
+            fontSize = 28.sp,
+            color = if (colorFlag)
+                colorResource(R.color.light_green) else Color.Black,
+            modifier = Modifier
+                .align(
+                    Alignment.CenterStart
+                )
+                .padding(start = 16.dp), fontWeight = FontWeight.Bold
+        )
+        Row(modifier = Modifier.align(Alignment.CenterEnd)) {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.camera),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            IconButton(onClick = {isSearching=true}) {
+                Icon(
+                    painter = painterResource(R.drawable.search),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.more),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    }
+    HorizontalDivider()
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun Test(){
+    TopBar("Updates",false)
+}
---
+++ app/src/main/java/com/icycodes/whatsapp/presentation/updatescreen/UpdateScreen.kt
@@ -0,0 +1,43 @@
+package com.icycodes.whatsapp.presentation.updatescreen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.icycodes.whatsapp.R
+import com.icycodes.whatsapp.presentation.bottomnavigation.BottomNavigation
+
+@Composable
+@Preview(showSystemUi = true)
+fun UpdateScreen() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = colorResource(R.color.light_green),
+                modifier = Modifier.size(65.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_photo_camera_24),
+                    contentDescription = null,
+                    tint = Color.White
+                )
+            }
+        }, bottomBar = {
+            BottomNavigation()
+        }
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+            TopBar("Updates", false)
+        }
+    }
+}
